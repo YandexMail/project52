0b4c42aff36134de3c498f0fee8f33fe11a0beff
thed636 <orionstation@yandex.ru>
2014-04-08 18:13:38 +0400

    RFC-like pseudo-SMTP implementation + pretty parsing out to console.

--- src/server/sync_server/connection.cc
+++ src/server/sync_server/connection.cc
@@ -1,30 +1,82 @@
 #include "connection.h"
 #include "../common/rfc822.h"
 
-void Connection::read() {
-    boost::system::error_code ec;
-    boost::asio::read_until(socket(), inBuf, "\r\n.\r\n", ec);
-    handleRead(ec);
+const std::string Connection::endOfData = "\r\n.\r\n";
+
+void Connection::start() {
+    writeGreeting();
+    serve();
+    writeGoodbye();
+    shutdown();
 }
 
-void Connection::handleRead(const boost::system::error_code& e) {
+void Connection::writeGreeting() {
+    reply("220 smtp11.mail.yandex.net sync_server SMTP\r\n");
+}
+
+void Connection::writeDataGreeting() {
+    reply( "354 Enter mail, end with \".\" on a line by itself\r\n");
+}
+
+void Connection::writeGoodbye() {
+    reply("221 smtp11.mail.yandex.net sync_server SMTP\r\n");
+}
+
+void Connection::serve() {
+    while( handleCommand() );
+}
+
+std::string Connection::getCommand() {
+    boost::asio::read_until(socket(), inBuf, "\r\n");
     std::istream s(&inBuf);
-    rfc822::parse(s);
-}
-
-void Connection::reply() {
-    boost::system::error_code ec;
-    std::ostream os(&outBuf);
-    os << int(200) << "\r\n";
-    boost::asio::write(socket(), outBuf, ec );
-    handleWrite(ec);
-}
-
-void Connection::handleWrite(const boost::system::error_code& e) {
-    if(!e) {
-        boost::system::error_code ignored_ec;
-        socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both,
-            ignored_ec);
+    std::string buf;
+    std::getline(s, buf);
+    if (!buf.empty() && buf.back() == '\r') {
+        buf.pop_back();
     }
+    return buf;
 }
 
+void Connection::readData() {
+    boost::asio::read_until(socket(), inBuf, endOfData);
+    handleData();
+}
+
+bool Connection::handleCommand() {
+    const std::string cmd = getCommand();
+    if( cmd == "DATA" ) {
+        writeDataGreeting();
+        readData();
+    } else if( cmd == "QUIT" ) {
+        return false;
+    } else {
+        reply();
+    }
+    return true;
+}
+
+void Connection::handleData() {
+    std::istream s(&inBuf);
+    try {
+        if( rfc822::parse(s) ) {
+            reply();
+        } else {
+            reply("451 rfc2822 violation\r\n");
+        }
+    } catch ( const std::exception & e ) {
+        reply(std::string("451 ") + e.what() + "\r\n");
+    }
+    //Cleanup buffer since the parser read until terminal "."
+    inBuf.consume(std::string(endOfData).length());
+}
+
+void Connection::reply( const std::string & str ) {
+    std::ostream os(&outBuf);
+    os << str << std::flush;
+    boost::asio::write(socket(), outBuf);
+}
+
+void Connection::shutdown() {
+    boost::system::error_code ignored_ec;
+    socket().shutdown(tcp::socket::shutdown_both, ignored_ec);
+}
