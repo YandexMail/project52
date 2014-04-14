6c342961a119f92ae140e794e0629f62b1bd2766
Nikki Chumakov <nikkikom@gmail.com>
2014-04-14 19:22:01 +0400

    Misc changes, see description
    1. Linking with boost static libraries.
    2. Added libboost_context in cmake
    3. Added Message archive as first arg in client.

--- src/client/client.cc
+++ src/client/client.cc
@@ -13,12 +13,12 @@ int main (int ac, char* av[])
 {
   try 
   {
-    if (ac != 3)
+    if (ac != 4)
     {
-      std::cout << "Usage: client <server_address> <port>\n";
+      std::cout << "Usage: client <message_archive_file> <server_address> <port>\n";
       return 1;
     }
-    message_generator::index index("mulca4fix.ammo");
+    message_generator::index index(av[1]);
 
     asio::io_service io_service;
     message_generator mgen( index.begin(), index.end() );
@@ -27,7 +27,7 @@ int main (int ac, char* av[])
     typedef client<io_model, message_generator> client_type;
 
     auto&& io_mod = std::make_shared<io_model> ();
-    io_mod->create<client_type> (io_service, av[1], av[2], mgen);
+    io_mod->create<client_type> (io_service, av[2], av[3], mgen);
 
     io_service.run ();
   }
